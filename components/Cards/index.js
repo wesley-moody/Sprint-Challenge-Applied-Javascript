623498940f8325bd5589be90aa3b648f5748cf36
wesley.moody <wgm3190@gmail.com>
2019-12-06 12:36:20 -0600

    finally got h1 to populate

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,47 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+
+
+function cardCreator()  {
+
+    // new elements
+    const card = document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgContainer = document.createElement('div')
+        const img = document.createElement('img');
+    const byLine = document.createElement('span');
+
+    // class list
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');  
+
+    //append children to parent
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    imgContainer.appendChild(img);
+    author.appendChild(byLine);
+
+    //text content
+    headline.textContent = title;
+    author.textContent = author;
+
+    return card;
+}
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles').then(res => {
+    const cardContainer = document.querySelector('.cards-container');
+    const holder = Object.values(res.data.articles);
+    holder.forEach(items => {
+        for (let obj of items) {
+            cardsContainer.appendChild(articleCards(obj));
+        }
+                
+    });
+})
